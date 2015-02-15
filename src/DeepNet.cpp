a6b4c5ca1ed503ce92a6407709a08b324241bd65
Shubham Gupta <shubham.gupta3112@gmail.com>
2015-02-15 15:41:59 -0800

    Display per 20 test iterations

--- src/DeepNet.cpp
+++ src/DeepNet.cpp
@@ -423,7 +423,7 @@ void train_network(const BridgeVector & bridges, const Corpus & corpus, const cn
   float t_pass;
 
   Timer t_total;
-  const int display_iter = 50;
+  const int display_iter = 50;  
 
   const size_t num_epochs = solver_param.max_iter();
   for (size_t epoch = 0; epoch < num_epochs; ++epoch) {
@@ -484,7 +484,6 @@ void train_network(const BridgeVector & bridges, const Corpus & corpus, const cn
       t_backward = t.elapsed();
       
       t_pass = t2.elapsed();
-
 
       if(batch % display_iter == 0){
         cout << "BATCH: " << batch << endl;
@@ -555,17 +554,20 @@ float test_network(const BridgeVector & bridges, const Corpus & corpus, const cn
 
   // num_mini_batches - 1, because we need one more iteration for the final mini batch
   // (the last mini batch may not be the same size as the rest of the mini batches)
+  float t_load;
+  float t_forward;
+  float t_pass;
   int total_accuracy = 0;
+  const int display_iter = 20;
   for (size_t batch = 0, corpus_batch_index = 0; batch < corpus.num_mini_batches - 1; ++batch,
       corpus_batch_index += corpus.mini_batch_size) {
-    cout << "BATCH: " << batch << endl;
 
     Timer t;
     Timer t2;
 
     fread(corpus.images->p_data, sizeof(DataType_SFFloat), corpus.images->n_elements, pFile);
-    std::cout << "Loading Time (seconds)     : " << t.elapsed() << std::endl;
-
+    t_load = t.elapsed();
+    t.restart();
     float * const mini_batch = corpus.images->physical_get_RCDslice(0);
     input_data->p_data = mini_batch;
 
@@ -579,16 +581,23 @@ float test_network(const BridgeVector & bridges, const Corpus & corpus, const cn
       //(*bridge)->p_output_layer->p_data_cube->reset_cube();
       (*bridge)->forward();
     }
-    std::cout << "Forward Pass Time (seconds) : " << t.elapsed() << std::endl;
+    t_forward = t.elapsed();
 
     float loss = (softmax->get_loss() / corpus.mini_batch_size);
     int batch_accuracy = find_accuracy(labels, softmax->p_output_layer->p_data_cube);
     total_accuracy += batch_accuracy;
 
-    std::cout << "\033[1;31m";
-    std::cout << "Total Time & Loss & Accuracy: " << t2.elapsed() << "    " << loss 
-              << "    " << 1.0*batch_accuracy/corpus.mini_batch_size;
-    std::cout << "\033[0m" << std::endl;
+    t_pass = t2.elapsed();
+
+    if(batch % display_iter == 0){
+      cout << "BATCH: " << batch << endl;
+      std::cout << "Loading Time (seconds)     : " << t_load << std::endl;
+      std::cout << "Forward Pass Time (seconds) : " << t_forward << std::endl;
+      std::cout << "\033[1;31m";
+      std::cout << "Total Time & Loss & Accuracy: " << t_pass << "    " << loss 
+                << "    " << 1.0*batch_accuracy/corpus.mini_batch_size;
+      std::cout << "\033[0m" << std::endl;
+    }
 
   }
   float acc = (1.0*total_accuracy/((corpus.num_mini_batches - 1)*corpus.mini_batch_size));
