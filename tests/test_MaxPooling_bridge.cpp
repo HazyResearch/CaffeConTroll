baa5b41bd7eabc73d7f9f432012854aac25a40b4
chrismre <chrismre@cs.stanford.edu>
2015-02-16 22:11:48 -0800

    fixing a particular bug in reading from file in the tests and updating gitignore

--- tests/test_MaxPooling_bridge.cpp
+++ tests/test_MaxPooling_bridge.cpp
@@ -97,10 +97,8 @@ TYPED_TEST(MaxPoolingBridgeTest, TestForward) {
   T output;
   int idx = 0;
   if (expected_output.is_open()) {
-    expected_output >> output;
-    while (!expected_output.eof()) {
+    while (expected_output >> output) {
       EXPECT_NEAR(this->data2->p_data[idx], output, EPS);
-      expected_output >> output;
       idx++;
     }
   }
@@ -141,12 +139,8 @@ TYPED_TEST(MaxPoolingBridgeTest, TestBackward) {
   T output;
   int idx = 0;
   if (expected_output.is_open()) {
-    expected_output >> output;
-    while (!expected_output.eof()) {
-      EXPECT_NEAR(this->grad1->p_data[idx], output, EPS);
-      expected_output >> output;
-      idx++;
-    }
+    while (expected_output >> output) 
+      EXPECT_NEAR(this->grad1->p_data[idx++], output, EPS);
   }else{
     FAIL();
   }
