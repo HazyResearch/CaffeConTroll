d6b76604612ba2ac67696397c7167347f663ec77
Stefan Hadjis <shadjis@stanford.edu>
2016-02-12 05:34:47 +0000

    Fixed fence post errors in multi threaded image processing

--- src/parser/corpus.h
+++ src/parser/corpus.h
@@ -22,7 +22,6 @@
 #include <google/protobuf/message_lite.h>
 #include <glog/logging.h>
 #include <thread>
-#include <functional>
 
 #include "parser.h"
 #include "lmdb.h"
@@ -122,14 +121,14 @@ class Corpus {
       float t_get = 0;
       float t_parse = 0;
       float t_process = 0;
-      std::thread threads[mini_batch_size - offset];
+      std::thread *threads[mini_batch_size];
       // timing test end
 
       cnn::Datum datum;
       int mdb_ret;
       // Note that the corpus owns the storage of its images
       float * const labels_data = labels->get_p_data();
-      int count = 0;
+      size_t count = 0;
       for (size_t b = offset; b < mini_batch_size; b++) { 
           Timer t;
           mdb_ret = mdb_cursor_get(mdb_cursor_, &mdb_key_, &mdb_value_, op);
@@ -139,7 +138,7 @@ class Corpus {
           }
 
           Timer t2;
-          threads[b] = std::thread([this, mdb_value_, b, labels_data](){
+          threads[b] = new std::thread([this, mdb_value_, b, labels_data](){
             cnn::Datum datum;
             datum.ParseFromArray(mdb_value_.mv_data, mdb_value_.mv_size);
 
@@ -158,16 +157,16 @@ class Corpus {
 
       // timing tests
       Timer t;
-      for (size_t b = offset; b < mini_batch_size; ++b){
-        threads[b].join();
+      for (size_t b = offset; b < offset + count; ++b){
+        threads[b]->join();
+	delete threads[b];
       }
       t_parse += t.elapsed();
       t_process += t.elapsed();
-      std::cout << "mdb_cursor_get: " << t_get << std::endl;
-      std::cout << "ParseFromArray: " << t_parse << std::endl;
-      std::cout << "process_image: " << t_process << std::endl;
+      //std::cout << "mdb_cursor_get: " << t_get << std::endl;
+      //std::cout << "ParseFromArray: " << t_parse << std::endl;
+      //std::cout << "process_image: " << t_process << std::endl;
       // timing test end
-
 
       return count;
     }
